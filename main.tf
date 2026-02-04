b0cb38272fb8a52a65f1217404cc2137500a37fd
cynthiaak <cynthia.khalil22@gmail.com>
2026-02-04 18:53:31 +0200

    Update EC2 instance to use latest Amazon Linux 2 AMI

--- main.tf
+++ main.tf
@@ -3,6 +3,17 @@ provider "aws" {
   region = "eu-north-1"
 }
 
+# Get latest Amazon Linux 2 AMI for this region
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
 # Security group allowing SSH and HTTP
 resource "aws_security_group" "web_sg" {
   name        = "web-sg"
@@ -32,9 +43,9 @@ resource "aws_security_group" "web_sg" {
 
 # EC2 instance
 resource "aws_instance" "web" {
-  ami           = "ami-0c02fb55956c7d316" # Amazon Linux 2 in us-east-1
-  instance_type = "t2.micro"
-  key_name      = "aub_ec2"          # 
+  ami           = data.aws_ami.amazon_linux.id
+  instance_type = "t3.micro"
+  key_name      = "aub_ec2"
 
   vpc_security_group_ids = [aws_security_group.web_sg.id]
 
